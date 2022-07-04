386d94b807ddf4c75dc60f4a38f28fbae3ef015e
Tr1xxter <cmango67@gmail.com>
2022-07-04 11:19:35 +0500

    Добавлено текстовое поле для уровня 1

--- src/Services/GamesRepository.cs
+++ src/Services/GamesRepository.cs
@@ -2,4 +2,25 @@ namespace thegame.Services;
 
 public class GamesRepository
 {
+    private const string Level1Static =
+        @"__XXXXX_
+XXX___X_
+X0____X_
+XXX__0X_
+X0XX__X_
+X_X_0_XX
+X__0__0X
+X___0__X
+XXXXXXXX";
+    private const string Level1Dynamic =
+        @"________
+________
+__P1____
+____1___
+_____1__
+_____1__
+________
+_1__111_
+________
+________";
 }
